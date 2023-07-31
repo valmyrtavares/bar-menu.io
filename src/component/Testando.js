e117e7004883de94f6acdc3a1c400e3c4103b0ab
Valmyr Tavares <valmyrtavares71@gmail.com>
2023-07-31 20:34:20 -0300

    Creating a component

---
+++ src/component/Testando.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Testando = () =>{
+    return (
+    <div>
+        <h1>Esse é um teste</h1>
+    </div>
+    )
+}
+export default Testando;
---
+++ src/component/lux.js
@@ -0,0 +1,8 @@
+
+
+const Lux = () => {
+  return <h1>Essa é a luz</h1>
+    
+  
+};
+export default Lux;
