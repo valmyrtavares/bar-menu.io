81def2a4164a2f82efd9f014a226d80193efded8
Valmyr Tavares <valmyrtavares71@gmail.com>
2023-10-24 17:25:41 -0300

    carrossel it works

--- src/component/carrosselImages.js
+++ src/component/carrosselImages.js
@@ -1,99 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './carrosselImages.css';
 import { getBtnData } from '../api/buttonApi';
 
-// Effect for auto-advancing
 function CarrosselImages() {
-  const [index, setIndex] = React.useState(0);
-  //const [images, setImages] = React.useState([]);
-  const [images, setImages] = React.useState([
-    'https://img.elo7.com.br/product/zoom/258B7CB/adesivo-parede-restaurante-prato-feito-comida-caseira-lenha-adesivo-restaurante-fritas-salada.jpg',
-    'https://img.cybercook.com.br/receitas/151/x-salada-3.jpeg',
-    'https://www.melhoresdestinos.com.br/wp-content/uploads/2020/09/comidas-tipicas-capa2019.jpg',
-  ]);
-  const [intervalId, setIntervalId] = React.useState(null);
-  const [carrosselImages, setCarrosselImages] = React.useState([]);
+  const [index, setIndex] = useState(0);
+  const [carrosselImages, setCarrosselImages] = useState([]);
 
-  // Functions
-  React.useEffect(() => {
-    fetchCarrosselImages();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getBtnData('item');
+      const carrosselImagesNovo = response
+        .filter((item) => item.carrossel === true)
+        .map((item) => item.image);
+      setCarrosselImages(carrosselImagesNovo);
+    };
 
-    const newInterval = setInterval(() => {
-      if (index == 2) {
-        setIndex(0);
-      } else {
-        setIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }
-    }, 4000);
-
-    return () => clearInterval(newInterval);
+    fetchData();
   }, []);
 
-  function forward() {
-    //  clearInterval(intervalId);
-    setIndex((prevIndex) => (prevIndex + 1) % images.length);
-  }
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % carrosselImages.length);
+    }, 2000);
 
-  function runCarrossel() {
-    const newInterval = setInterval(() => {
-      if (index == 2) {
-        setIndex(0);
-      } else {
-        setIndex((prevIndex) => prevIndex + 1);
-      }
-    }, 4000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [carrosselImages]);
 
-    // return () => clearInterval(newInterval);
-  }
+  const forward = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % carrosselImages.length);
+  };
 
-  function backward() {
-    // clearInterval(intervalId);
-    if (index === 0) {
-      setIndex(images.length - 1);
-    } else {
-      setIndex((prevIndex) => (prevIndex - 1) % images.length);
-    }
-  }
-
-  async function fetchCarrosselImages() {
-    console.log('funcionando');
-    const response = await getBtnData('item');
-    const carrosselImagesNovo = await response
-      .filter((item) => item.carrossel === true)
-      .map((item) => item.image);
-    // console.log(carrosselImagesNovo);
-    setCarrosselImages(carrosselImagesNovo);
-    console.log(carrosselImages);
-    // runCarrossel();
-
-    // if (!response.ok) {
-    //   throw new Error('something  went wrong!');
-    // }
-    // let y;
-    // console.log(response);
-  }
+  const backward = () => {
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? carrosselImages.length - 1 : prevIndex - 1
+    );
+  };
 
   return (
-    <>
-      <div className="content-carrossel">
-        {carrosselImages && <p>{JSON.stringify(carrosselImages)}</p>} //display
-        the array
-        <div className="carrossel">
-          <img src={images[index]} />
-        </div>
-        <div className="d-flex justify-content-between mt-3">
-          <button className="btn btn-success" onClick={backward}>
-            Anterior
-          </button>
-          <button className="btn btn-success" onClick={forward}>
-            Seguinte
-          </button>
-        </div>
+    <div className="content-carrossel">
+      <div className="carrossel">
+        {carrosselImages.length > 0 && (
+          <img src={carrosselImages[index]} alt={`Image ${index}`} />
+        )}
       </div>
-      <h1>{index}</h1>
-      <button onClick={fetchCarrosselImages}>Teste</button>
-    </>
+      <div className="d-flex justify-content-between mt-3">
+        <button className="btn btn-success" onClick={backward}>
+          Anterior
+        </button>
+        <button className="btn btn-success" onClick={forward}>
+          Seguinte
+        </button>
+      </div>
+    </div>
   );
 }
+
 export default CarrosselImages;
-//
