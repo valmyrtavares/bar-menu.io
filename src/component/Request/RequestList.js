fe78c2f23f13c470ed20255d8044b8220d46dd24
valmyrtavares <valmyrtavares71@gmail.com>
2024-08-15 12:09:08 -0300

    styled RequestList

--- src/component/Request/RequestList.js
+++ src/component/Request/RequestList.js
@@ -20,11 +20,11 @@ const RequestList = () => {
           <div key={item.id} className="request">
             <div className="customer">
               <h3>Cliente</h3>
-              <p>
+              <p className="customer-name">
                 <span>Nome</span> {item.name}
               </p>
               <p>
-                <span>Valor</span> {item.finalPriceRequest}
+                <span>Valor Total R$ </span> {item.finalPriceRequest},00
               </p>
               <p>
                 {" "}
@@ -33,16 +33,22 @@ const RequestList = () => {
             </div>
             <div>
               {item.request &&
-                item.request.map((dishe) => (
+                item.request.map((dishe, index) => (
                   <div className="dishes">
                     <div>
+                      <h3>item {index + 1}</h3>
                       <p>{dishe.name}</p>
-                      <p>{dishe.finalPrice}</p>
+                      <p>R$ {dishe.finalPrice},00</p>
                     </div>
                     <div className="sidedishes">
+                      <p>
+                        <h3>Adicionais</h3>
+                      </p>
                       {dishe.sideDishes &&
                         dishe.sideDishes.map((sidedishe, index) => (
-                          <p>{sidedishe}</p>
+                          <div>
+                            <p>{sidedishe}</p>
+                          </div>
                         ))}
                     </div>
                   </div>
