94ddbc52a12992108dddb2bd0d7feb9affa2e0e3
ValmyrTavares <valmyrtavares71@gmail.com>
2024-08-28 15:23:46 -0300

    just is possible use a old user if it exists otherwise the createCustomer componente is called

--- src/component/ProtectedUser.js
+++ src/component/ProtectedUser.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { getBtnData } from "../api/Api";
 
 const ProtectedUser = () => {
-  const token = localStorage.getItem('userMenu');
+  const [isAuthenticated, setIsAuthencicated] = React.useState(null);
 
-  return token ? <Navigate to="/menu" /> : <Navigate to="/create-customer" />;
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const { id } = JSON.parse(localStorage.getItem("userMenu"));
+      const token = await fetchUser(id);
+      if (token && token.length > 0) {
+        setIsAuthencicated(true);
+      } else {
+        setIsAuthencicated(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const fetchUser = async (id) => {
+    const data = await getBtnData("user");
+    const token = data.filter((item) => item.id === id);
+    return token;
+  };
+
+  if (isAuthenticated === null) {
+    // Você pode renderizar um spinner ou algo enquanto espera o resultado da autenticação
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? (
+    <Navigate to="/menu" />
+  ) : (
+    <Navigate to="/create-customer" />
+  );
 };
 
 export default ProtectedUser;
