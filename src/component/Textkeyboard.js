eb07858cdca0dd6b33a7c919a1b0ff1e0e3fe432
ValmyrTavares <valmyrtavares71@gmail.com>
2024-09-12 11:35:43 -0300

    Refatoring the keyboard of toten

--- src/component/Textkeyboard.js
+++ src/component/Textkeyboard.js
@@ -8,7 +8,87 @@ const TextKeyboard = ({ addCharacter, closeKeyboard, id }) => {
 
   return (
     <div>
-      <div className="text-keyboard-container">
+      <div className="text-keyboard-line-container">
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("0")}
+        >
+          0
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("1")}
+        >
+          1
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("2")}
+        >
+          2
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("3")}
+        >
+          3
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("4")}
+        >
+          4
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("5")}
+        >
+          5
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("6")}
+        >
+          6
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("7")}
+        >
+          7
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("8")}
+        >
+          8
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("9")}
+        >
+          9
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("@")}
+        >
+          @
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("-")}
+        >
+          -
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick("_")}
+        >
+          _
+        </div>
+      </div>
+      <div className="text-keyboard-line-container">
         <div
           className="text-character"
           onClick={() => handleCharacterClick("q")}
@@ -71,6 +151,14 @@ const TextKeyboard = ({ addCharacter, closeKeyboard, id }) => {
         </div>
         <div
           className="text-character"
+          onClick={() => handleCharacterClick("p")}
+        >
+          Bcksp
+        </div>
+      </div>
+      <div className="text-keyboard-line-container">
+        <div
+          className="text-character"
           onClick={() => handleCharacterClick("a")}
         >
           a
@@ -125,6 +213,8 @@ const TextKeyboard = ({ addCharacter, closeKeyboard, id }) => {
         >
           l
         </div>
+      </div>
+      <div className="text-keyboard-line-container">
         <div
           className="text-character"
           onClick={() => handleCharacterClick("z")}
@@ -175,20 +265,28 @@ const TextKeyboard = ({ addCharacter, closeKeyboard, id }) => {
         >
           p
         </div>
-        <div
-          className="text-character"
-          onClick={() => handleCharacterClick(" ")}
-        >
-          Esp
-        </div>
+      </div>
+      <div className="text-keyboard-line-container">
         <div
           className="text-character"
           onClick={() => handleCharacterClick("clearField")}
         >
           limpar
         </div>
+        <div
+          className="text-character space"
+          onClick={() => handleCharacterClick(" ")}
+        >
+          Esp
+        </div>
         <div className="text-character" onClick={closeKeyboard}>
           Enter
+        </div>
+        <div
+          className="text-character"
+          onClick={() => handleCharacterClick(".com")}
+        >
+          .com
         </div>
       </div>
     </div>
