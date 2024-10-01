ee5ff547b6a4c193377bb5f9799227788be88d07
ValmyrTavares <valmyrtavares71@gmail.com>
2024-10-01 17:18:58 -0300

    customer is deleted if it is anonymous and do not buy nothing during 2 minutes

--- src/Pages/MainMenu.js
+++ src/Pages/MainMenu.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CarrosselImages from "../component/carouselComponent";
 import NestedBtn from "../component/nestedBtn";
-import { getBtnData } from "../api/Api";
+import { getBtnData, getOneItemColleciton, deleteData } from "../api/Api";
 import MenuButton from "../component/menuHamburguerButton";
 import RequestModal from "../component/Request/requestModal.js";
 import { Link, useNavigate } from "react-router-dom";
@@ -15,7 +15,7 @@ function MainMenu() {
   // const [displayForm, setDisplayForm] = React.useState(false);
   const [menuButton, setMenuButton] = React.useState([]);
   const [dishes, setDishes] = React.useState([]);
-  const [nameClient, serNameClient] = React.useState("");
+  const [nameClient, setNameClient] = React.useState("");
   const containerRef = React.useRef(null);
   const global = React.useContext(GlobalContext);
   const [logoutAdminPopup, setLogoutAdminPopup] = React.useState(false);
@@ -24,7 +24,7 @@ function MainMenu() {
 
   React.useEffect(() => {
     if (!global.authorizated) {
-      ChecLogin();
+      CheckLogin();
     }
     const fetchData = async () => {
       try {
@@ -36,7 +36,7 @@ function MainMenu() {
         setDishes(dataItem);
         grabClient();
       } catch (error) {
-        console.error("Error fetching data", error);
+        console.error("Erro fetching data", error);
       }
     };
     fetchData();
@@ -47,12 +47,12 @@ function MainMenu() {
       if (logoutAdminPopup) {
         localStorage.removeItem("userMenu");
         global.setAuthorizated(false);
-        ChecLogin();
+        CheckLogin();
       }
       setLogoutAdminPopup(true);
     }
   };
-  async function ChecLogin() {
+  async function CheckLogin() {
     const userId = await CheckUser("userMenu");
     navigate(userId);
   }
@@ -60,12 +60,34 @@ function MainMenu() {
   function grabClient() {
     if (localStorage.hasOwnProperty("userMenu")) {
       const nameCustomer = JSON.parse(localStorage.getItem("userMenu"));
+      console.log("nameCustomer   ", nameCustomer);
+      if (nameCustomer.name === "anonimo") {
+        deleteAnonymousWithnoItem(nameCustomer.id);
+      }
+
       let firstName = nameCustomer.name.split(" ")[0];
       firstName =
         firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
-      serNameClient(firstName);
+      setNameClient(firstName);
     }
   }
+
+  const deleteAnonymousWithnoItem = (id) => {
+    setTimeout(async () => {
+      debugger;
+      try {
+        const data = await getOneItemColleciton("user", id);
+        if (!data?.request || data?.request.length === 0) {
+          await deleteData("user", id);
+          localStorage.removeItem("userMenu");
+          global.setAuthorizated(false);
+          CheckLogin();
+        }
+      } catch (error) {
+        console.error("Erro ao buscar e deletar dados", error);
+      }
+    }, 10000);
+  };
 
   return (
     <>
