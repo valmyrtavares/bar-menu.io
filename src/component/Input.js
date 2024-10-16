9a304f6f87b8d0b35dad63b92c1111e9b536718b
valmyr tavares <valmyrtavares71@gmail.com>
2024-10-16 12:07:48 -0300

    changing font size witn screen size

--- src/component/Input.js
+++ src/component/Input.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 const Input = ({ label, fieldFocus, id, ...props }) => {
+
+  const inputStyle = {
+    fontSize: window.innerWidth > 900 ? '37px' : '16px', // Estilo condicional
+    padding: '10px',
+    borderRadius: '4px',
+   
+  };
+  
   return (
     <div className="mb-3">
-      <label className="form-label" htmlFor={label}>
+      <label style={inputStyle} className="form-label" htmlFor={label}>
         {label}
       </label>
-      <input ref={fieldFocus} className="form-control" id={id} {...props} />
+      <input ref={fieldFocus}style={inputStyle} className="form-control" id={id} {...props} />
     </div>
   );
 };
