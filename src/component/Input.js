9d46265767c51b8f7b926d224f8f1ed8a6bfc5bd
valmyr tavares <valmyrtavares71@gmail.com>
2024-10-18 14:09:14 -0300

    scroll bar in AddDishesForm component

--- src/component/Input.js
+++ src/component/Input.js
@@ -1,20 +1,24 @@
 import React from "react";
 
 const Input = ({ label, fieldFocus, id, ...props }) => {
-
   const inputStyle = {
-    fontSize: window.innerWidth > 900 ? '33px' : '16px', // Estilo condicional
-    padding: '10px',
-    borderRadius: '4px',
-   
+    fontSize: window.innerWidth > 900 ? "25px" : "16px", // Estilo condicional
+    padding: "10px",
+    borderRadius: "4px",
   };
-  
+
   return (
     <div className="mb-3">
       <label style={inputStyle} className="form-label" htmlFor={label}>
         {label}
       </label>
-      <input ref={fieldFocus}style={inputStyle} className="form-control" id={id} {...props} />
+      <input
+        ref={fieldFocus}
+        style={inputStyle}
+        className="form-control"
+        id={id}
+        {...props}
+      />
     </div>
   );
 };
