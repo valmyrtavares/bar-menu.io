611b5ad79f9749877c3ec7b3351cb7921f5385ce
valmyrtavaes <valmyrtavares71@gmail.com>
2024-10-28 15:46:36 -0300

    change local settings

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter,
   HashRouter,
   Route,
   Routes,
   Navigate,
-} from "react-router-dom";
-import MainMenu from "./Pages/MainMenu";
-import AddButtonForm from "./Forms/AddButtonForm";
-import AddSideDishesForm from "./Forms/AddSideDishesForm";
-import FormItem from "./Forms/AddDishesForm";
-import Header from "./component/header";
-import Signup from "./Forms/Login/signup";
-import Login from "./Forms/Login/login";
-import Admin from "./Pages/FormMenu";
-import Protected from "./component/Protected";
-import FormFrontImage from "./Forms/formFrontImage";
-import ListToEditAndDelete from "./Forms/ListToEditAndDelete";
+} from 'react-router-dom';
+import MainMenu from './Pages/MainMenu';
+import AddButtonForm from './Forms/AddButtonForm';
+import AddSideDishesForm from './Forms/AddSideDishesForm';
+import FormItem from './Forms/AddDishesForm';
+import Header from './component/header';
+import Signup from './Forms/Login/signup';
+import Login from './Forms/Login/login';
+import Admin from './Pages/FormMenu';
+import Protected from './component/Protected';
+import FormFrontImage from './Forms/formFrontImage';
+import ListToEditAndDelete from './Forms/ListToEditAndDelete';
 // import ProtectedUser from "./component/ProtectedUser";
-import CreateCustomer from "./Forms/Login/createCustomer";
-import RequestModal from "./component/Request/requestModal";
-import ScreenStylesForm from "./Forms/ScreenStylesForm";
-import WelcomeSaluteForm from "./Forms/WelcomeSaluteForm";
-import RequestListToBePrepared from "./component/Request/RequestListToBePrepared";
-import OrderQueue from "./component/orderQueue";
-import RequestList from "./component/Request/RequestList";
-import RecipeDish from "./Forms/recipeDishForm";
-import CustomerList from "./component/Customers/customerList";
-import NoLog from "./Forms/Login/NoLog";
-import PrintRequestCustomer from "./component/Request/PrintRequestCustomer";
-import RequestManagementModule from "./component/Request/requestManagementModule";
+import CreateCustomer from './Forms/Login/createCustomer';
+import RequestModal from './component/Request/requestModal';
+import ScreenStylesForm from './Forms/ScreenStylesForm';
+import WelcomeSaluteForm from './Forms/WelcomeSaluteForm';
+import RequestListToBePrepared from './component/Request/RequestListToBePrepared';
+import OrderQueue from './component/orderQueue';
+import RequestList from './component/Request/RequestList';
+import RecipeDish from './Forms/recipeDishForm';
+import CustomerList from './component/Customers/customerList';
+import NoLog from './Forms/Login/NoLog';
+import PrintRequestCustomer from './component/Request/PrintRequestCustomer';
+import RequestManagementModule from './component/Request/requestManagementModule';
 
-import "./style.css";
+import './style.css';
 
 function App() {
-  const basename = "/bar-menu.io";
-  // const basename = "/";
+  // const basename = "/bar-menu.io";
+  const basename = '/';
   return (
     <div className="ultra-wrapper">
       <BrowserRouter basename={basename}>
@@ -74,8 +74,8 @@ function App() {
           <Route path="/admin/frontimage" element={<FormFrontImage />} />
           <Route path="/admin/styles" element={<ScreenStylesForm />} />
           <Route path="/admin/welcome" element={<WelcomeSaluteForm />} />
-          <Route path="*" element={<Navigate to="/bar-menu.io" replace />} />
-          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+          {/*<Route path="*" element={<Navigate to="/bar-menu.io" replace />} />*/}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </div>
