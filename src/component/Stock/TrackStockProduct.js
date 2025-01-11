4310f4a990718e6c7be4c9b4f00e13098bc66c74
valmyrtavaes <valmyrtavares71@gmail.com>
2025-01-11 13:17:09 -0300

    The tables of trackStockProducts has header fixed in top of table

--- src/component/Stock/TrackStockProduct.js
+++ src/component/Stock/TrackStockProduct.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getBtnData, deleteData } from '../../api/Api';
-import products from '../../assets/styles/TrackStockProduct.module.scss';
+import style from '../../assets/styles/TrackStockProduct.module.scss';
 import DefaultComumMessage from '../Messages/DefaultComumMessage';
 import EditFormStockProduct from './EditFormStockProduct';
 import AdjustmentRecords from './AdjustmentRecords';
@@ -54,7 +54,7 @@ const TrackStockProduct = () => {
   };
 
   return (
-    <div className={products.containerTrackStockproduct}>
+    <div className={style.containerTrackStockproduct}>
       {showEditForm && (
         <EditFormStockProduct
           fetchStock={fetchStock}
@@ -62,7 +62,7 @@ const TrackStockProduct = () => {
           setShowEditForm={setShowEditForm}
         />
       )}
-      <div className={products.containerAdjustmentScreen}>
+      <div className={style.containerAdjustmentScreen}>
         {showAdjustmentRecords && (
           <AdjustmentRecords
             eventLogData={eventLogData}
@@ -74,42 +74,48 @@ const TrackStockProduct = () => {
       <Link to="/admin/admin">
         <Title mainTitle="Estoque" />
       </Link>
-      <table striped bordered hover>
-        <tr>
-          <th>Produto</th>
-          <th>Custo de Estoque</th>
-          <th>Estoque em volume</th>
-          <th>Quantidade de embalagens</th>
-          <th>Editar</th>
-        </tr>
-        {stock &&
-          stock.length > 0 &&
-          stock.map((item, index) => (
-            <tr key={index}>
-              <td onClick={() => usageHistory(item)}>{item.product}</td>
-              <td>R$ {Number(item.totalCost).toFixed(2)}</td>
-              <td>
-                {Number(item.totalVolume).toFixed(2)}
-                {item.unitOfMeasurement}
-              </td>
-              {showWarningDeletePopup && (
-                <DefaultComumMessage
-                  msg={`Você está prestes a excluir ${excludeStockItem.product}`}
-                  item={excludeStockItem}
-                  onConfirm={deleteStockItem}
-                  onClose={() => setShowWarningDeltePopup(false)}
-                />
-              )}
-              <td>{Number(item.amount).toFixed(2)}</td>
-              <td
-                style={{ cursor: 'pointer' }}
-                onClick={() => editStockItem(item)}
-              >
-                Editar
-              </td>
+      <div className={style.tableStockContainer}>
+        <table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Produto</th>
+              <th>Custo de Estoque</th>
+              <th>Estoque em volume</th>
+              <th>Quantidade de embalagens</th>
+              <th>Editar</th>
             </tr>
-          ))}
-      </table>
+          </thead>
+          <tbody>
+            {stock &&
+              stock.length > 0 &&
+              stock.map((item, index) => (
+                <tr key={index}>
+                  <td onClick={() => usageHistory(item)}>{item.product}</td>
+                  <td>R$ {Number(item.totalCost).toFixed(2)}</td>
+                  <td>
+                    {Number(item.totalVolume).toFixed(2)}
+                    {item.unitOfMeasurement}
+                  </td>
+                  {showWarningDeletePopup && (
+                    <DefaultComumMessage
+                      msg={`Você está prestes a excluir ${excludeStockItem.product}`}
+                      item={excludeStockItem}
+                      onConfirm={deleteStockItem}
+                      onClose={() => setShowWarningDeltePopup(false)}
+                    />
+                  )}
+                  <td>{Number(item.amount).toFixed(2)}</td>
+                  <td
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => editStockItem(item)}
+                  >
+                    Editar
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
