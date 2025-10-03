73f5ee36d826552ab802146e6ff8fd29f745c5ee
valmyrtavaes <valmyrtavares71@gmail.com>
2025-10-03 14:21:33 -0300

    env  was implemented

--- src/config-firebase/firebase.js
+++ src/config-firebase/firebase.js
@@ -3,16 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC7sJ3wQY40ZXNvwn-QcbNr51R1Gjui_1E',
-  authDomain: 'react-bar-67f33.firebaseapp.com',
-  databaseURL: 'https://react-bar-67f33-default-rtdb.firebaseio.com',
-  projectId: 'react-bar-67f33',
-  storageBucket: 'react-bar-67f33.appspot.com',
-  messagingSenderId: '621276654255',
-  appId: '1:621276654255:web:c90ba2bc75df7ae1edc25a',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
