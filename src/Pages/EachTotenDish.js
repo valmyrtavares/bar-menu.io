744b670b9acbdbb56928e035b9a0846bb3135917
valmyrtavaes <valmyrtavares71@gmail.com>
2025-11-21 18:25:16 -0300

    message unavaiable in each dish

--- src/Pages/EachTotenDish.js
+++ src/Pages/EachTotenDish.js
@@ -3,15 +3,15 @@ import style from '../assets/styles/EachTotenDish.module.scss';
 const EachTotenDish = ({ item, index, preparedRequest }) => {
   return (
     <div
-      onClick={() => preparedRequest(item)}
+      onClick={item.lowAmountRawMaterial ? null : () => preparedRequest(item)}
       className={style.itemContainer}
       style={{ animationDelay: `${index * 0.1}s` }}
       key={item.id || index} // Evita recriação desnecessária
     >
-      <div className={style.image} onClick={() => preparedRequest(item)}>
+      <div className={style.image}>
         <img src={item.image} alt="" />
       </div>
-      <h3>{item.title}</h3>
+      <h3>{item.lowAmountRawMaterial ? 'indisponivel' : item.title}</h3>
       <div className={style.text}>
         <p>{item.comment}</p>
       </div>
