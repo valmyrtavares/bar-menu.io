7204c70454cd8d62b21559367925c71f3ad7e0bb
valmyrtavaes <valmyrtavares71@gmail.com>
2026-02-24 14:03:56 -0300

    feat: the sumary expenses popup is fixed in the central screen with scrooll bar if it exceeds the max height.

--- src/component/Payment/ExpensesManegementList/SumaryExpensesListPopup.js
+++ src/component/Payment/ExpensesManegementList/SumaryExpensesListPopup.js
@@ -9,87 +9,89 @@ const SumaryExpensesListPopup = ({ oneExpense, setOpenSumaryPopup }) => {
     }
   }, [oneExpense]);
   return (
-    <div className={style.containerSumaryExpenses}>
-      <CloseBtn setClose={setOpenSumaryPopup} />
-      <h1>Despesa Completa</h1>
-      <main>
-        <div>
-          <p>
-            Nome da Despesa<span>: {oneExpense.name}</span>
-          </p>
-          <p>
-            Categoria<span>: {oneExpense.category}</span>
-          </p>
-          <p>
-            {' '}
-            Confirmação<span>: {oneExpense.confirmation}</span>
-          </p>
-          <p>
-            Data de Pagamento<span>: {oneExpense.dueDate}</span>
-          </p>
-          <p>
-            Despesa Paga em <span>: {oneExpense.paymentDate}</span>
-          </p>
-          <p>
-            Valor Total<span>: {oneExpense.value}</span>
-          </p>
-          <p>
-            Quantidade de items
-            <span>
-              : {oneExpense && oneExpense.items && oneExpense.items.length}
-            </span>
-          </p>
-        </div>
-        <div>
-          {oneExpense.items &&
-            oneExpense.items.length > 0 &&
-            oneExpense.items.map((item) => (
-              <div className={style.containerItem}>
-                <h4>Produto {item.product}</h4>
-                <p>
-                  Custo por unidade
-                  <span> {item.CostPerUnit}</span>
-                </p>
-                <p>
-                  Custo Total
-                  <span> {item.totalCost}</span>
-                </p>
-                <p>
-                  Volume por unidade
-                  <span>
-                    {' '}
-                    {item.volumePerUnit}
-                    {item.unitOfMeasurement}
-                  </span>
-                </p>
-                <p>
-                  Quantdade de volumes
-                  <span> {item.amount}</span>
-                </p>
-                <p>
-                  Volume Total
-                  <span>
-                    {' '}
-                    {item.totalVolume}
-                    {item.unitOfMeasurement}
-                  </span>
-                </p>
-                <p>
-                  Volume Anterior
-                  <span>
-                    {' '}
-                    {item.currentAmountProduct}
-                    {item.unitOfMeasurement}
-                  </span>
-                </p>
-                <p>
-                  Informações adicionais
-                  <span> {item.adjustmentExpenseNote}</span>
-                </p>
-              </div>
-            ))}
-        </div>
-      </main>
+    <div className={style.overlay}>
+      <div className={style.containerSumaryExpenses}>
+        <CloseBtn setClose={setOpenSumaryPopup} />
+        <h1>Despesa Completa</h1>
+        <main>
+          <div className={style.detailsWrapper}>
+            <p>
+              Nome da Despesa<span>: {oneExpense.name}</span>
+            </p>
+            <p>
+              Categoria<span>: {oneExpense.category}</span>
+            </p>
+            <p>
+              {' '}
+              Confirmação<span>: {oneExpense.confirmation}</span>
+            </p>
+            <p>
+              Data de Pagamento<span>: {oneExpense.dueDate}</span>
+            </p>
+            <p>
+              Despesa Paga em <span>: {oneExpense.paymentDate}</span>
+            </p>
+            <p>
+              Valor Total<span>: {oneExpense.value}</span>
+            </p>
+            <p>
+              Quantidade de items
+              <span>
+                : {oneExpense && oneExpense.items && oneExpense.items.length}
+              </span>
+            </p>
+          </div>
+          <div className={style.itemsWrapper}>
+            {oneExpense.items &&
+              oneExpense.items.length > 0 &&
+              oneExpense.items.map((item, index) => (
+                <div key={index} className={style.containerItem}>
+                  <h4>Produto {item.product}</h4>
+                  <p>
+                    Custo por unidade
+                    <span> {item.CostPerUnit}</span>
+                  </p>
+                  <p>
+                    Custo Total
+                    <span> {item.totalCost}</span>
+                  </p>
+                  <p>
+                    Volume por unidade
+                    <span>
+                      {' '}
+                      {item.volumePerUnit}
+                      {item.unitOfMeasurement}
+                    </span>
+                  </p>
+                  <p>
+                    Quantdade de volumes
+                    <span> {item.amount}</span>
+                  </p>
+                  <p>
+                    Volume Total
+                    <span>
+                      {' '}
+                      {item.totalVolume}
+                      {item.unitOfMeasurement}
+                    </span>
+                  </p>
+                  <p>
+                    Volume Anterior
+                    <span>
+                      {' '}
+                      {item.currentAmountProduct}
+                      {item.unitOfMeasurement}
+                    </span>
+                  </p>
+                  <p>
+                    Informações adicionais
+                    <span> {item.adjustmentExpenseNote}</span>
+                  </p>
+                </div>
+              ))}
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
